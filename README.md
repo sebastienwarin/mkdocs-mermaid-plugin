024e1bfe01600be251a916842889a10ceabad52e
Sébastien Warin <sebastien@warin.fr>
2020-01-21 00:46:44 +0100

    Custom initialization options from mkdocs.yml

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A MkDocs plugin that render meraid graph to mermaid style
 Install the package with pip:
 
 ```bash
-pip install mkdocs-mermaid-plugin
+pip install git+https://github.com/sebastienwarin/mkdocs-mermaid-plugin
 ```
 
 ## Usage
@@ -21,11 +21,29 @@ plugins:
     - markdownmermaid
 
 extra_javascript:
-    - https://unpkg.com/mermaid@7.1.2/dist/mermaid.min.js
+    - https://unpkg.com/mermaid@8.4.5/dist/mermaid.min.js
 ```
 
 > **Note:** Don't forget to include the mermaid.min.js (local or remotely) in your `mkdocs.yml`
 
+To override the [default options](https://mermaid-js.github.io/mermaid/#/mermaidAPI?id=mermaidapi), declare them in you `mkdocs.yml` file.
+
+For example :
+
+```yaml
+plugins:
+    - markdownmermaid:
+        init_options:
+            - startOnLoad: true
+            - theme: "forest"
+            - themeCSS: ".node rect { fill: red; }"
+            - flowchart:
+                - curve: "cardinal"
+                - useMaxWidth: true
+
+```
+
+More information about mermaid in the [Mermaid documentation](https://mermaid-js.github.io/mermaid/)
 More information about plugins in the [MkDocs documentation][mkdocs-plugins]
 
 
--- markdownmermaid/plugin.py
+++ markdownmermaid/plugin.py
@@ -1,5 +1,6 @@
 from mkdocs.plugins import BasePlugin
 from bs4 import BeautifulSoup
+import json
 
 try:
     unicode
@@ -8,6 +9,15 @@ except NameError:
     unicode = str
 
 class MarkdownMermaidPlugin(BasePlugin):
+
+    def transform_options(self, source, toJson = True):
+        options = {}
+        for opt in source:
+            option = {}
+            for key, value in opt.items():
+                option[key] = self.transform_options(value, False) if isinstance(value, list) else value
+            options.update(option)
+        return json.dumps(options) if toJson else options
    
     def on_post_page(self, output_content, config, **kwargs):
         soup = BeautifulSoup(output_content, 'html.parser')
@@ -20,7 +30,10 @@ class MarkdownMermaidPlugin(BasePlugin):
 
         if mermaids:
             new_tag = soup.new_tag("script")
-            new_tag.string = "mermaid.initialize();"
+            if 'init_options' in self.config:
+                new_tag.string = "mermaid.initialize({config});".format(config=self.transform_options(self.config['init_options']))
+            else:
+                new_tag.string = "mermaid.initialize({startOnLoad:true});"
             soup.body.append(new_tag)
             
         return unicode(soup)
